a42d0e86f3e42586eeba3f0d597ddc76940335e7
SaraiKayCook <scook23@wvstateu.edu>
2025-04-07 14:34:03 -0400

    Add files via upload

---
+++ Lab_9.py
@@ -0,0 +1,24 @@
+"""
+Lab_9.py
+Purpose: To become familiar with Python
+Author: Sarai Cook
+Date: April 7, 2025
+"""
+#The most interesting and useful part of basic Python to me is the ability to
+#edit lists even after they are made. This can be demonstrated in the code
+#below.
+
+#Make a list of colors
+colors = ["purple", "pink", "orange"]
+
+#Print the second item on the list
+print(colors[1])
+
+#Add a color
+colors.append("green")
+
+#Remove a color
+colors.remove("pink")
+
+#Print the new list
+print(colors)
